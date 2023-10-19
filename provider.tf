47854893df02d3c256aa540110aeb1d2d3d0d6af
rupinderjit ghuman <rupinderjit.ghuman@phsa.ca>
2023-10-19 12:16:37 -0700

    update

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "3.75.0"
     }
   }
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name = var.bkstrgrg
     storage_account_name = var.bkstrg
     container_name = var.bkcontainer
